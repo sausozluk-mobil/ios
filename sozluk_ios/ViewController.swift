bfced06152bde9e8797732e486c95533d3d1769d
mahmut.akyol <mahmut.akyol@outlook.com.tr>
2018-04-27 01:46:32 +0300

    Initial Commit

---
+++ sozluk_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sozluk_ios
+//
+//  Created by mahmut akyol on 27.04.2018.
+//  Copyright © 2018 mahmut akyol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
